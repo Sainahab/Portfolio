4a93d5640affc3ff53694f6375f825e65759dd80
Sain Ahab <96540199+Sainahab@users.noreply.github.com>
2025-07-05 23:21:16 +0100

    optimized

--- PHPMailer/src/PHPMailer.php
+++ PHPMailer/src/PHPMailer.php
@@ -4250,8 +4250,8 @@ class PHPMailer
      * Automatically inlines images and creates a plain-text version by converting the HTML,
      * overwriting any existing values in Body and AltBody.
      * Do not source $message content from user input!
-     * $basedir is prepended when handling relative URLs, e.g. <img src="/images/a.png"> and must not be empty
-     * will look for an image file in $basedir/images/a.png and convert it to inline.
+     * $basedir is prepended when handling relative URLs, e.g. <img src="/images/a.webp"> and must not be empty
+     * will look for an image file in $basedir/images/a.webp and convert it to inline.
      * If you don't provide a $basedir, relative paths will be left untouched (and thus probably break in email)
      * Converts data-uri images into embedded attachments.
      * If you don't want to apply these transformations to your HTML, just set Body and AltBody directly.
---
+++ assets/js/scriptold.js
@@ -0,0 +1,258 @@
+// //Navigation Bar Effects On Scroll
+// window.addEventListener("scroll", function(){
+//     const header = document.querySelector("header");
+
+// });    
+
+// //Services Section - Modal
+// const serviceModals = document.querySelectorAll(".service-modal");
+// const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+// const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+// var modal = function(modalClick){
+//     serviceModals[modalClick].classList.add("active");
+// }
+
+// learnmoreBtns.forEach((learnmoreBtn, i) => {
+//     learnmoreBtn.addEventListener("click", () => {
+//         modal(i);
+//     });
+// });
+
+// modalCloseBtns.forEach((modalCloseBtn) => {
+//     modalCloseBtn.addEventListener("click", () => {
+//         serviceModals.forEach((modalView) => {
+//             modalView.classList.remove("active");
+//         });
+//     });
+// });
+
+
+
+
+// //Our Client - swiper
+
+// var swiper = new Swiper(".client-swiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+
+// //Website dark/light theme
+// const themeBtn = document.querySelector(".theme-btn");
+
+// themeBtn.addEventListener("click", () => {
+//     document.body.classList.toggle("dark-theme");
+//     themeBtn.classList.toggle("sun");
+
+//     localStorage.setItem("saved-theme", getCurrentTheme());
+//     localStorage.setItem("saved-icon", getCurrentIcon());
+// });
+
+// const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+// const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+// const savedTheme = localStorage.getItem("saved-theme");
+// const savedIcon = localStorage.getItem("saved-icon");
+
+// if(savedTheme){
+//     document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+//     themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+// }
+
+// //Scroll To Top Button
+// const scrollToTopBtn = document.querySelector(".scrollToTop-btn");
+
+// window.addEventListener("scroll", function(){
+//     scrollToTopBtn.classList.toggle("active", window.scrollY > 500)
+// });
+
+// scrollToTopBtn.addEventListener("click", () => {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// });
+
+// //Navigation menu items active on page scroll
+// window.addEventListener("scroll", () => {
+//     const sections = document.querySelectorAll("section");
+//     const scrollY = window.pageYOffset;
+
+//     sections.forEach(current => {
+//         let sectionHeight = current.offsetHeight;
+//         let sectionTop = current.offsetTop - 50;
+//         let id = current.getAttribute("id");
+
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+//         }
+//         else{
+//             document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+//         }
+//     });
+// });
+// // Responsive navigation menu toggle
+// const menuBtn = document.querySelector(".nav-menu-btn");
+// const closeBtn = document.querySelector(".nav-close-btn");
+// const navigation = document.querySelector(".navigation");
+
+// menuBtn.addEventListener("click", () => {
+//     navigation.classList.add("active");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//     navigation.classList.remove("active");
+// });
+
+// // Close the menu when clicking on any menu item
+// const menuItems = document.querySelectorAll(".navigation a");
+
+// menuItems.forEach((item) => {
+//     item.addEventListener("click", () => {
+//         navigation.classList.remove("active");
+//     });
+// });
+
+// // Close the menu when clicking outside of it
+// document.addEventListener("click", (event) => {
+//     if (!navigation.contains(event.target) && event.target !== menuBtn) {
+//         navigation.classList.remove("active");
+//     }
+// });
+//Navigation Bar Effects On Scroll
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+
+});    
+
+//Services Section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+
+
+
+//Our Client - swiper
+
+var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+//Website dark/light theme
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if(savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
+//Scroll To Top Button
+const scrollToTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollToTopBtn.classList.toggle("active", window.scrollY > 500)
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+//Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        }
+        else{
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+});
+// Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+// Close the menu when clicking on any menu item
+const menuItems = document.querySelectorAll(".navigation a");
+
+menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
+
+// Close the menu when clicking outside of it
+document.addEventListener("click", (event) => {
+    if (!navigation.contains(event.target) && event.target !== menuBtn) {
+        navigation.classList.remove("active");
+    }
+});
